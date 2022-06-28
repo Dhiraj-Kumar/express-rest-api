b865142a077076aafb379bde20fcf6558f5b022f
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2022-06-28 15:00:30 +0530

    updated port

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ mongoose.connection.once('open', () => {
     console.log(err);
 });
 
-app.listen(8000, () => {
-    console.log('Server is running');
+let port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
